d105f0fe501154604dc57e2b4f9b6869bb59fcc7
Emeliaisdumb <66752943+emeliagit@users.noreply.github.com>
2022-12-17 04:10:01 +0000

    Add the full installer
    Haven't tested it yet, might be buggy. Please report issues/open pull requests if it is, I'm happy to fix it, since I install Fedora on basically everything, I'll probably use this script fairly often.

---
+++ FullEmeliaFedoraInstall.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+echo "Hello! This little script here is gonna do some stuff, install some software to make your install of Fedora more complete and comfortable to install with little hassle."
+echo "Beware, this script does enable RPM Fusion's nonfree repository for drivers and whatnot, feel free to edit it to your liking. It will also require a root password, and your user password later to change your shell. Feel free to change that if you don't like it."
+# upgrade computer before doing anything
+sudo dnf upgrade 
+# enable RPM Fusion's free and nonfree repos 
+sudo dnf install \
+  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install \
+  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+# Append some options that I like to dnf.conf (dnf's config file, located in /etc/dnf)
+sudo echo "gpgcheck=True
+installonly_limit=3
+clean_requirements_on_remove=True
+best=False
+skip_if_unavailable=True
+max_parallel_downloads=6
+defaultyes=True" >> /etc/dnf.conf
+# install some software I like to use, personally
+sudo dnf install lolcat alacritty fastfetch flatpak ark gnome-tweaks gnome-extensions-app fish zsh
+# change default shell to Fish, as I like using Fish to interact with my OS. Bash for scripting, though. Fish sucks for scripting lol
+chsh -s /bin/fish
+#  add Flathub repository to Flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# install some flatpaks I like 
+flatpak install librewolf flatseal discord blanket kget
+# run a quick Fastfetch 'cause Fastfetch is cool, then display ending message
+fastfetch | lolcat
+echo "All done! Hopefully everything worked, sorry if it didn't. Enjoy your Emelia-ified Fedora installation. :)"
